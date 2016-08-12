f93c553b4bafdf57a60cb7c7086ea7570d4830e4
Lionas <lionas.watanabe@gmail.com>
2016-08-13 00:08:12 +0900

    WIP:実装中

--- LionasFoodAndDrinkReminder/Lang/jp.lua
+++ LionasFoodAndDrinkReminder/Lang/jp.lua
@@ -1,34 +1,19 @@
-﻿-- Advanced Disable Controller UI
+﻿-- Lionas's Food & Drink Reminder
 -- Author: Lionas
 -- Japanese specific file
 
 -- Description
-ZO_CreateStringId("ADCUI_DESCRIPTION", "ゲームコントローラUIを無効化とコンパス位置の調整")
+ZO_CreateStringId("LIO_FADR_DESCRIPTION", "食べ物と飲み物をゲームコントローラUIを無効化とコンパス位置の調整")
 
--- lock compass
-ZO_CreateStringId("ADCUI_LOCK_TITLE", "位置の固定")
-ZO_CreateStringId("ADCUI_LOCK_TOOLTIP", "コンパスの位置を固定/解除します")
+-- notification
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_HOUR_MIN", "残り<<1>>時間<<2>>分で<<3>>の効果が消えます")
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_MIN", "残り<<1>>分で<<2>>の効果が消えます")
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_CLOSED", "<<1>>の効果がまもなく消えます")
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRED", "<<1>>の効果が切れました！")
 
--- compass scale
-ZO_CreateStringId("ADCUI_SCALE_TITLE", "拡大率")
-ZO_CreateStringId("ADCUI_SCALE_TOOLTIP", "コンパスの拡大率を設定します")
-
--- compass width
-ZO_CreateStringId("ADCUI_WIDTH_TITLE", "幅")
-ZO_CreateStringId("ADCUI_WIDTH_TOOLTIP", "コンパスの幅を設定します")
-
--- compass height
-ZO_CreateStringId("ADCUI_HEIGHT_TITLE", "高さ")
-ZO_CreateStringId("ADCUI_HEIGHT_TOOLTIP", "コンパスの高さを設定します")
-
--- center pin text scale
-ZO_CreateStringId("ADCUI_LABEL_SCALE_TITLE", "ラベルの拡大率")
-ZO_CreateStringId("ADCUI_LABEL_SCALE_TOOLTIP", "ピンの上に表示される文字の拡大率を設定します")
-
--- disable addon
-ZO_CreateStringId("ADCUI_USE_CONTROLLER_UI", "コントローラUIを使用する")
+ZO_CreateStringId("LIO_FADR_SHOULD_EAT_DRINK", "食べ物/飲み物の効果を発動してください")
 
 -- keybinds
-ZO_CreateStringId("SI_BINDING_NAME_ADCUI_ACTIVATE", "設定画面の表示")
-ZO_CreateStringId("SI_BINDING_NAME_ADCUI_USE_GAMEPAD", "コントローラUIを使用する")
+ZO_CreateStringId("SI_BINDING_NAME_LIO_FADR_ACTIVATE", "有効")
+
 
--- LionasFoodAndDrinkReminder/LionasFoodAndDrinkReminder.lua
+++ LionasFoodAndDrinkReminder/LionasFoodAndDrinkReminder.lua
@@ -3,7 +3,9 @@
 
 LioFADR = {
   name = "LionasFoodAndDrinkReminder",
-  notified = {},
+  notifyRemain = {}, -- 残り時間通知済みリスト
+  notifyClosed = {}, -- 残り時間が迫っている通知済みリスト
+  notifyThresholdSecs = 180, -- 通知する閾値(秒)
   default = {
       -- default values
   },  
@@ -17,14 +19,17 @@ local UPDATE_INTERVAL_REGISTER_NAME = LioFADR.name .. "_Update"
 local UPDATE_ONCE_REGISTER_NAME = LioFADR.name .. "_UpdateOnce"
 local SAVED_PREFS_NAME = LioFADR.name .. "_SavedPrefs"
 
-local UPDATE_INTERVAL_MSEC = 500
+local UPDATE_INTERVAL_MSEC = 1000
+local HOUR_PER_SECS = 3600
+local MIN_PER_SECS = 60
+local THRESHOLD_EXPIRE_SECS = 60
 
 -- Initialize preferences
-local function initializePrefs()
+--local function initializePrefs()
 
-  LioFADR.savedVariables = ZO_SavedVars:New(SAVED_PREFS_NAME, 1, nil, LioFADR.default)
+--  LioFADR.savedVariables = ZO_SavedVars:New(SAVED_PREFS_NAME, 1, nil, LioFADR.default)
   
-end
+--end
 
 -- 実装仕様
 -- ダンジョンに入った時のみ、「食べ物」と「飲み物」の効果があるかどうかのチェック
@@ -32,24 +37,162 @@ end
 -- 選択できる食べ物または飲み物がクイックスロットにない時は「使用可能な食べ物または飲み物がありません！」とvanilla UI上に表示する
 -- 効果がある時は、「残りx時間 x分（1分未満の時はもうすぐ）で食べ物または飲み物の効果が消えます」と vanilla UI上で表示する
 -- 効果が切れた時は、「食べ物または飲み物の効果が切れました！」とvanilla UI上で表示する。その際、戦闘中でない場合は、（効果がない時）と同じ処理を実行。
--- 食べ物または飲み物の残り時間が少なくなる（3分未満、設定変更可能）と、Vanilla UI上で「xxx（食べ物の名前、できればアイコン付き）の効果がまもなく切れます」と表示する
+-- 食べ物または飲み物の残り時間が少なくなる（1分未満、設定変更可能）と、Vanilla UI上で「xxx（食べ物の名前、できればアイコン付き）の効果がまもなく切れます」と表示する
 
--- Cyclic check
-local function LioFADR.onUpdate()
 
-	if(IsUnitInDungeon(PLAYER_TAG)) then
-		-- ダンジョンの中
-		scanBuffs()
-		
-	else
-		-- ダンジョンの外
-		-- 何もしない	
-	end
+
+
+
+-- 通知
+local function notify(message, buffName)
+  
+  -- チャット欄に通知
+  d(message)
+  
+  -- Vanilla UIに通知
+  CENTER_SCREEN_ANNOUNCE:AddMessage(999, CSA_EVENT_COMBINED_TEXT, SOUNDS.CHAMPION_POINTS_COMMITTED, message, buffName, nil, nil, nil, nil, CSA_OPTION_SUPPRESS_ICON_FRAME),
+  
+end
+
+
+-- 通知済みテーブルから削除
+local function removeFromTable(table, abilityId)
+  
+  while (i <= #table) do
+    
+    if(table[i] == abilityId) then
+      table.remove(table, i)
+    else
+      i = i + 1
+    end
+    
+  end  
+  
+end
+
+
+-- 残り時間に応じて通知するメッセージを変える
+local function buildNotifyMessage(remainSec, buffName)
+  
+  if (remainSec >= HOUR_PER_SECS)　then
+    -- 1時間以上効果がある時は、「残りx時間x分で<食べ物または飲み物>の効果が消えます」
+  
+    -- メッセージの作成
+    local hours = remainSec / HOUR_PER_SECS
+    local mins = (remainSec % HOUR_PER_SECS) / MIN_PER_SECS
+    
+    return zo_strformat(GetString(LIO_FADR_NEAR_EXPIRE_HOUR_MIN), hours, mins, buffName)
+          
+  end
+  
+  if (remainSec >= MIN_PER_SECS) and (remainSec < HOUR_PER_SECS) then
+    -- 1分以上1時間未満の効果がある時は、「残りx分で<食べ物または飲み物>の効果が消えます」
+    
+    -- メッセージの作成
+    local mins = remainSec / MIN_PER_SECS
+    return zo_strformat(GetString(LIO_FADR_NEAR_EXPIRE_MIN), mins, buffName)
+
+  end
+
+  if (remainSec > 0) and (remasinSec < MIN_PER_SECS) then
+    -- 1分未満の効果がある時は、「<食べ物または飲み物>の効果がまもなく消えます」
+    return zo_strformat(GetString(LIO_FADR_NEAR_EXPIRE_CLOSED), buffName)
+    
+  else
+    -- <食べ物または飲み物>の効果が切れました！
+    return zo_strformat(GetString(LIO_FADR_NEAR_EXPIRED), buffName)
+    
+  end
+  
+end
+
+
+-- 残り時間を通知
+local function notifyRemainTime(remainSec, abilityId, buffName)
+
+  if (remainSec <= LioFADR.notifyThresholdSecs)　and (not in_array(abilityId, LioFADR.notifyRemain)) then
+    -- 残り時間が指定時間以下になったら通知する
+    
+    -- 通知
+    notify(buildNotifyMessage(remainSec, buffName), buffName)
+          
+    -- 通知済みテーブルに追加と削除
+    table.insert(LioFADR.notifyRemain, abilityId)
+    removeFromTable(LioFADR.notifyClosed, abilityId)
+          
+  elseif (remainSec < THRESHOLD_EXPIRE_SECS) and (not in_array(abilityId, LioFADR.notifyClosed)) then
+    -- 指定時間を切ったら最終通知
+    
+    -- 通知
+    nofity(buildNotifyMessage(remainSec, buffName), buffName)
+    
+    -- 通知済みテーブルに追加と削除
+    table.insert(LioFADR.notifyClosed, abilityId)
+    removeFromTable(LioFADR.notifyRemain, abilityId)
+
+  end
+    
+end
+
+
+-- 有効なバフかどうかのチェック
+local function getEnableBuff(effectType, statusEffectType, timeStarted, timeEnding, abilityType, canClickOff)
+  
+	return (effectType == BUFF_EFFECT_TYPE_BUFF) and 
+				 (statusEffectType == STATUS_EFFECT_TYPE_NONE) and 
+				 ((timeEnding - timeStarted) > 0) and
+				 (in_array( abilityType, { ABILITY_TYPE_NONE, ABILITY_TYPE_BONUS } )) and
+				 canClickOff
+         
+end
+
+
+-- 残り時間が延長されたバフがある場合は通知していないことにする
+local function extendRemainTime(remainSec, abilityId)
+  
+  if (remainSec >= LioFADR.notifyThresholdSecs and in_array(abilityId, LioFADR.notifyRemain)) then
+    removeFromTable(LioFADR.notifyRemain, abilityId)
+  end
+  
+    if (remainSec >= LioFADR.notifyThresholdSecs and in_array(abilityId, LioFADR.notifyClosed)) then
+    removeFromTable(LioFADR.notifyClosed, abilityId)
+  end
+  
+end
+
+
+-- 効果が切れたアビリティの通知
+local function notifyExpiredBuffs(currentBuffs)
+
+  local expiredBuffs = {}
+    
+  -- 残り時間が迫っているバフの一覧
+  for _, id in pairs(LioFADR.notifyClosed) do
+    
+    -- 現在のバフ一覧に入っていないものはもうバフが切れているので、期限切れ一覧に追加
+    if (not in_array(id, currentBuffs)) then
+      table.insert(expiredBuffs id)
+    end
+    
+  end
+  
+  -- 効果が切れたバフの一覧
+  for _, id in pairs(expiredBuffs) do
+    
+    -- 残り時間が迫っているバフ一覧から削除
+    removeFromTable(LioFADR.notifyClosed, id)
+    
+    -- 通知
+    String buffName = GetAbilityName(id)
+    notify(buildNotifyMessage(0, buffName), buffName)
+    
+  end
 
 end
 
+
 -- Scan buffs
-local function LioFADR.scanBuffs()
+local function scanBuffs()
 
 	-- 「食べ物」と「飲み物」の効果があるかどうかのチェック
 	local buffsNum = GetNumBuffs(PLAYER_TAG)
@@ -82,33 +225,44 @@ local function LioFADR.scanBuffs()
 			-- 残り時間の算出
 			local remainSec = timeEnding - (GetGameTimeMilliseconds() / 1000)
 			
+      -- 残り時間が延長されたバフがある場合は通知していないことにする
+      extendRemainTime(remainSec, abilityId)
+      
+      -- 残り時間の通知
+      notifyRemainTime(remainSec, abilityId, buffName)
 		
-		end	
-	
+    end	
+  
+  end
+
+  -- 効果が切れたバフの通知
+  notifyExpiredBuffs(currentBuffs)
+  
+  -- 効果がない時で、戦闘中でない場合、自動的に指定のスロットを選択し、「Q（コントローラの時は上ボタン）を押して食べ物 または 飲み物の効果を発動してください」のメッセージをVanilla UI上で表示する
+  --GetString(LIO_FADR_SHOULD_EAT_DRINK)
+
+
+
+end
+
+
+-- Cyclic check
+local function onUpdate()
+
+	if(IsUnitInDungeon(PLAYER_TAG)) then
+		-- ダンジョンの中
+		scanBuffs()
+		
+	else
+		-- ダンジョンの外
+		-- 何もしない	
 	end
 
 end
 
--- 有効なバフかどうかのチェック
-local function LioFADR.getEnableBuff(effectType, statusEffectType, timeStarted, timeEnding, abilityType, canClickOff)
-	return (effectType == BUFF_EFFECT_TYPE_BUFF) and 
-				 (statusEffectType == STATUS_EFFECT_TYPE_NONE) and 
-				 ((timeEnding - timeStarted) > 0) and
-				 (in_array( abilityType, { ABILITY_TYPE_NONE, ABILITY_TYPE_BONUS } )) and
-				 canClickOff
-end
-
--- player deactivated
-local function LioFADR.onPlayerDeactivated()
-
-  EVENT_MANAGER:UnregisterForUpdate(UPDATE_INTERVAL_REGISTER_NAME)
-  EVENT_MANAGER:UnregisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_DEACTIVATED)
-  
-end
-
 
 -- player activated
-local function LioFADR.onPlayerActivated()
+local function onPlayerActivated()
 
   -- unregist event handler
   --EVENT_MANAGER:UnregisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_ACTIVATED)
@@ -117,52 +271,37 @@ local function LioFADR.onPlayerActivated()
   LoadLAM2Panel()
   
   -- regist handler  
-  EVENT_MANAGER:RegisterForUpdate(UPDATE_INTERVAL_REGISTER_NAME, UPDATE_INTERVAL_MSEC, LioFADR.onUpdate)
-  EVENT_MANAGER:RegisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_DEACTIVATED, LioFADR.onPlayerDeActivated)
+  EVENT_MANAGER:RegisterForUpdate(UPDATE_INTERVAL_REGISTER_NAME, UPDATE_INTERVAL_MSEC, onUpdate)
+  EVENT_MANAGER:RegisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_DEACTIVATED, onPlayerDeActivated)
 
 end
 
+
+-- player deactivated
+local function onPlayerDeactivated()
+
+  EVENT_MANAGER:UnregisterForUpdate(UPDATE_INTERVAL_REGISTER_NAME)
+  EVENT_MANAGER:UnregisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_DEACTIVATED)
+  
+end
+
+
 -- OnLoad
-local function LioFADR.onLoad(event, addon)
+local function onLoad(event, addon)
   
   if(addon ~= LioFADR.name) then
     return
   end
   
-  initializePrefs()
+  -- 保存領域の初期化
+--  initializePrefs()
 
-  -- regist handler
-  EVENT_MANAGER:RegisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_ACTIVATED, LioFADR.onPlayerActivated)  
-  
-  -- unregist handler
+  -- regist & unregist handler
+  EVENT_MANAGER:RegisterForEvent(PLAYER_ACTIVATED_REGISTER_NAME, EVENT_PLAYER_ACTIVATED, onPlayerActivated)  
   EVENT_MANAGER:UnregisterForEvent(ADD_ON_LOADED_REGISTER_NAME, EVENT_ADD_ON_LOADED)
  
 end
 
--- Update variables
-local function onUpdateVars()
-
---d("onUpdateVars")
-  
-  local anchor, point, rTo, rPoint, offsetx, offsety = ZO_CompassFrame:GetAnchor() 
-    
-  if((offsetx ~= ADCUI.savedVariables.x and offsetx ~= ADCUI.default.anchorOffsetX)
-      or 
-     (offsety ~= ADCUI.savedVariables.y and offsety ~= ADCUI.default.anchorOffsetY)) then
-    
-    ADCUI.savedVariables.x = offsetx
-    ADCUI.savedVariables.y = offsety
-    ADCUI.savedVariables.point = point
-  
-    if(rPoint ~= nil) then
-      ADCUI.savedVariables.point = rPoint
-    end
-  
---d("x="..ADCUI.savedVariables.x..", y="..ADCUI.savedVariables.y..", point="..ADCUI.savedVariables.point)
-  
-  end
-  
-end  
 
 -- Regist event handler
-EVENT_MANAGER:RegisterForEvent(ADD_ON_LOADED_REGISTER_NAME, EVENT_ADD_ON_LOADED, LioFADR.onLoad)
+EVENT_MANAGER:RegisterForEvent(ADD_ON_LOADED_REGISTER_NAME, EVENT_ADD_ON_LOADED, onLoad)
