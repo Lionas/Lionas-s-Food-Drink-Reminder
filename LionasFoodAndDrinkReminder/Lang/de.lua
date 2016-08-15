e0f641d6bf329d632867af1b076dbea759f25cc6
Lionas <lionas.watanabe@gmail.com>
2016-08-15 23:47:21 +0900

    ドイツ語のフォントが潰れてしまっていた問題の修正

--- LionasFoodAndDrinkReminder/Lang/de.lua
+++ LionasFoodAndDrinkReminder/Lang/de.lua
@@ -7,9 +7,9 @@ ZO_CreateStringId("LIO_FADR_ENABLE", " ist aktiv.")
 ZO_CreateStringId("LIO_FADR_DISABLE", " ist nicht aktiv.")
 
 -- notification
-ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_HOUR_MIN", "Die Wirkung der Mahlzeit l?uft in <<1>>h <<2>>min aus.")
-ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_MIN", "Die Wirkung der Mahlzeit l?uft in <<1>>min aus.")
-ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_CLOSED", "Die Wirkung der Mahlzeit l?uft bald aus.")
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_HOUR_MIN", "Die Wirkung der Mahlzeit läuft in <<1>>h <<2>>min aus.")
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_MIN", "Die Wirkung der Mahlzeit läuft in <<1>>min aus.")
+ZO_CreateStringId("LIO_FADR_NEAR_EXPIRE_CLOSED", "Die Wirkung der Mahlzeit läuft bald aus.")
 ZO_CreateStringId("LIO_FADR_NEAR_EXPIRED", "Du hast den Wirkung der Mahlzeit verloren!")
 
 ZO_CreateStringId("LIO_FADR_SHOULD_EAT_DRINK", "Es ist keine Mahlzeit aktiv!")
@@ -17,15 +17,13 @@ ZO_CreateStringId("LIO_FADR_SHOULD_EAT_DRINK", "Es ist keine Mahlzeit aktiv!")
 -- menus
 ZO_CreateStringId("LIO_FADR_DESCRIPTION", "Dieses AddOn zeigt dir den Status deiner Mahlzeit an..")
 ZO_CreateStringId("LIO_FADR_ENABLE_TITLE", "Aktiviere Meldungen")
-ZO_CreateStringId("LIO_FADR_ENABLE_TOOLTIP", "Wenn du ?ber den Zustand deiner Mahlzeit informiert werde m?chtest, aktiviere diese Einstellung.")
-ZO_CreateStringId("LIO_FADR_NOTIFY_THRESHOLD_TITLE", "Schwelle f?r Erinnerung in [min]")
-ZO_CreateStringId("LIO_FADR_NOTIFY_THRESHOLD_TOOLTIP", "Du wirst ?ber den Zustand der Mahlzeit infomiert, sobald dieser Wert unterschritten wird.")
+ZO_CreateStringId("LIO_FADR_ENABLE_TOOLTIP", "Wenn du über den Zustand deiner Mahlzeit informiert werde möchtest, aktiviere diese Einstellung.")
+ZO_CreateStringId("LIO_FADR_NOTIFY_THRESHOLD_TITLE", "Schwelle für Erinnerung in [min]")
+ZO_CreateStringId("LIO_FADR_NOTIFY_THRESHOLD_TOOLTIP", "Du wirst über den Zustand der Mahlzeit infomiert, sobald dieser Wert unterschritten wird.")
 ZO_CreateStringId("LIO_FADR_NOTIFY_ONLY_IN_DUNGEON_TITLE", "Meldungen nur innerhalb Verlies")
-ZO_CreateStringId("LIO_FADR_NOTIFY_ONLY_IN_DUNGEON_TOOLTIP", "Wenn du nur ?ber den Zustand der Mahlzeit in einem Verlies informiert werden m?chtest, aktiviere diese Einstellung.")
-
+ZO_CreateStringId("LIO_FADR_NOTIFY_ONLY_IN_DUNGEON_TOOLTIP", "Wenn du nur über den Zustand der Mahlzeit in einem Verlies informiert werden möchtest, aktiviere diese Einstellung.")
 ZO_CreateStringId("LIO_FADR_ENABLE_NOTIFY_TO_CHAT_TITLE", "Geben Sie zum Chat bekannt")
-ZO_CreateStringId("LIO_FADR_ENABLE_NOTIFY_TO_CHAT_TOOLTIP", "Wenn Sie zum Chat ?ber den Staat Ihrer Mahlzeit informiert werden, w?rde wollen, diese Einstellung aktiviert.")
+ZO_CreateStringId("LIO_FADR_ENABLE_NOTIFY_TO_CHAT_TOOLTIP", "Wenn Sie ermöglichen, um über den Staat der Mahlzeit bekannt zu geben, um zu plaudern, überprüfen Sie es bitte.")
 
 -- keybinds
 ZO_CreateStringId("SI_BINDING_NAME_LIO_FADR_ACTIVATE", "Aktiviere/Deaktiviere Meldung")
-
