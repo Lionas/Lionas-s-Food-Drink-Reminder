c1ae0040881ce42e591f272a0044530d7ed442e0
Lionas <lionas.watanabe@gmail.com>
2016-08-18 01:17:32 +0900

    アビリティ対応表追加

---
+++ ability/merge_ability.lua
@@ -0,0 +1,204 @@
+[66568] = {
+  ["k"]  = "Unknown",
+  ["jp"] = "最大マジカを増加させる",
+  ["en"] = "Increase Max Magicka",
+  ["de"] = "gestärkte Magicka^f",
+},
+[66576] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "最大スタミナを増加させる",
+  ["en"] = "Increase Max Stamina",
+  ["de"] = "Erhöht Eure maximale Ausdauer",
+},
+[66586] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "体力再生",
+  ["en"] = "Health Recovery",
+  ["de"] = "Lebensregeneration^f",   
+},
+[66594] = 
+{
+  ["k"] = "Drink",
+  ["jp"] = "スタミナ再生",
+  ["en"] = "Stamina Recovery",
+  ["de"] = "Ausdauerregeneration^f",
+},
+[66125] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "体力の最大値を上昇させる",
+  ["en"] = "Increase Max Health",
+  ["de"] = "Erhöht Euer maximales Leben",
+},
+[66128] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "最大マジカを増加させる",
+  ["en"] = "Increase Max Magicka",
+  ["de"] = "Erhöht Eure maximale Magicka",
+},
+[66127] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "",
+  ["en"] = "",
+  ["de"] = "Erhöht Eure maximale Magicka",
+},
+[66137] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "マジカ再生",
+  ["en"] = "Magicka Recovery",
+  ["de"] = "Erhöht Eure Magickaregeneration",
+},
+[62554] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "体力回復",
+  ["en"] = "Restore Health",
+  ["de"] = "Leben wiederherstellen",
+},
+[66132] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "体力再生",
+  ["en"] = "Health Recovery",
+  ["de"] = "Erhöht Eure Lebensregeneration",
+},
+[62574] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "スタミナ回復",
+  ["en"] = "Restore Stamina",
+  ["de"] = "Ausdauer wiederherstellen^N",
+},
+[62576] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "マジカ回復",
+  ["en"] = "Restore Magicka",
+  ["de"] = "Magicka wiederherstellen",
+},
+[66130] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "最大スタミナを増加させる",
+  ["en"] = "Increase Max Stamina",
+  ["de"] = "Erhöht Eure maximale Ausdauer",
+},
+[68412] = 
+{
+  ["k"]  = "Unknown",
+  ["jp"] = "最大スタミナを増加させる",
+  ["en"] = "Increase Max Stamina",
+  ["de"] = "Erhöht Eure maximale Ausdauer",
+},
+                    [66590] = 
+                    {
+                        ["k"] = "Drink",
+                        ["jp"] = "マジカ再生",
+                    },
+                    [66551] = 
+                    {
+                        ["jp"] = "体力の最大値を上昇させる",
+                    },
+                    [68409] = 
+                    {
+                        ["jp"] = "スタミナ回復",
+                    },
+                    [68413] = 
+                    {
+                        ["jp"] = "最大マジカを増加させる",
+                    },
+                    [68416] = 
+                    {
+                        ["jp"] = "全主要ステータスの再生",
+                    },
+                    [61259] = 
+                    {
+                        ["k"] = "Food",
+                        ["jp"] = "体力の最大値を上昇させる",
+                    },
+                    [61261] = 
+                    {
+                        ["k"] = "Food",
+                        ["jp"] = "最大スタミナを増加させる",
+                    },
+                    [66254] = 
+                    {
+                        ["jp"] = "体力回復",
+                    },
+                    [66141] = 
+                    {
+                        ["jp"] = "スタミナ再生",
+                    },
+                    [60770] = 
+                    {
+                        ["jp"] = "マジカ再生",
+                    },
+                    [60771] = 
+                    {
+                        ["jp"] = "マジカ再生",
+                    },
+                    [60773] = 
+                    {
+                        ["jp"] = "スタミナ回復",
+                    },
+                    [61345] = 
+                    {
+                      ["k"] = "Drink",
+                        ["jp"] = "マジカとスタミナ再生",
+                    },
+                    [61325] = 
+                    {
+                        ["k"] = "Drink",
+                        ["jp"] = "マジカ再生",
+                    },
+                    [61294] = 
+                    {
+                        ["k"] = "Food",
+                        ["jp"] = "最大マジカとスタミナをアップする",
+                    },
+                    [61260] = 
+                    {
+                        ["k"] = "Food",
+                        ["jp"] = "最大マジカを増加させる",
+                    },
+                    [61257] = 
+                    {
+                        ["k"] = "Food",
+                        ["jp"] = "最大体力とマジカを増加させる",
+                    },
+                    [61255] = 
+                    {
+                        ["k"] = "Food",
+                        ["jp"] = "最大体力とスタミナを増加させる",
+                    },
+                    [61322] = 
+                    {
+                        ["k"] = "Drink",
+                        ["jp"] = "体力再生",
+                    },
+                    [61328] = 
+                    {
+                        ["k"] = "Drink",
+                        ["jp"] = "スタミナ再生",
+                    },
+                    [61335] = 
+                    {
+                        ["k"] = "Drink",
+                        ["jp"] = "体力とマジカ再生",
+                    },
+                    [61340] = 
+                    {
+                        ["k"] = "Drink",
+                        ["jp"] = "体力とスタミナ再生",
+                    },
+                    [68407] = 
+                    {
+                        ["jp"] = "マジカ回復",
+                    },
+
+
